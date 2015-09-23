2bbea1b0ef5a2f8443bbece580fece088a429617
timqian <timqian92@qq.com>
2015-09-23 21:15:07 +0800

    bundle

--- index.js
+++ index.js
@@ -6,21 +6,24 @@ require("babel-core/polyfill"); //??????????????????没有它会出错  http://b
 
 let code = window.location.search.slice(6);
 console.log(typeof code);
+
 if (code.length < 1) {
   window.location.href='https://github.com/login/oauth/authorize?client_id=4e4f2621589085b864d7';
 }
 
-// const postConfig = {
-//   params:{
-//     code,
-//     client_id: '4e4f2621589085b864d7',
-//     client_secret: 'd990379890dd26d973f227304d4c88b10528c76b',
-//   },
-// };
-// (async function(){
-//   const res = await axios.post('https://github.com/login/oauth/access_token',postConfig).catch(function (err) {console.log(err);});
-//   console.log(res);
-// })();
+const postConfig = {
+  params:{
+    code,
+    client_id: '4e4f2621589085b864d7',
+    client_secret: 'd990379890dd26d973f227304d4c88b10528c76b',
+  },
+};
+console.log(postConfig);
+(async function(){
+  const res = await axios.post('https://github.com/login/oauth/access_token',postConfig)
+    .catch(err => {console.log(err);});
+  console.log(res);
+})();
 
 
 let data = [];
