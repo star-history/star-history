12768ecdc06a629a10d6ff3095bfcad5a6c5109d
kevinpjk <kevinpjk@umich.edu>
2020-11-26 10:36:46 -0500

    Added generate color functionality and save colorSets

--- website/src/draw.js
+++ website/src/draw.js
@@ -1,11 +1,14 @@
 import chartXkcd from 'chart.xkcd';
 
+var colorSets = [];
+
 /**
  * draw star history graph based on data
  * @param {String} datasets example [{label:'tj/koa', data:[{x:'2016-6-12', y:12}, ...]}, ...]
  */
 export default function draw(datasets) {
   const svg = document.querySelector('#chart svg');
+  generateColor();
   new chartXkcd.XY(svg, {
     title: 'Star history',
     yLabel: 'Github stars',
@@ -20,6 +23,13 @@ export default function draw(datasets) {
       showLine: true,
       timeFormat: 'MM/DD/YYYY',
       dotSize: 0.5,
+      dataColors: colorSets,
     },
   })
+}
+
+function generateColor() {
+  var color = '#';
+  color += Math.floor(Math.random()*16777215).toString(16);
+  colorSets.append(color);
 }
