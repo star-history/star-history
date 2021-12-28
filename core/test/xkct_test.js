c0e7aa7cc01d5bab9188fc7d2a2bac409d2915ec
STEVEN <imrealleonardo@gmail.com>
2021-12-28 13:15:08 +0800

    Revert "Bytebase master (#97)" (#98)
    This reverts commit 710d2c77aaeaf220c0161384adf9da4cd9212fa7.

---
+++ core/test/xkct_test.js
@@ -0,0 +1,1 @@
+import xkcd from '../xkcd';
--- src/extension/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import Popup from "./Popup.vue";
-import "../css/index.css";
-import "../css/popup.css";
-
-createApp(Popup).mount("#app");
