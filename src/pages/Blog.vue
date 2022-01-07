8884859624e73a72e482e8f1c8366be8e5a80b94
email <imrealleonardo@gmail.com>
2022-01-07 18:07:11 +0800

    update blog styles

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -2,42 +2,30 @@
   <div class="relative w-full h-auto min-h-screen overflow-auto flex flex-col">
     <Header />
     <div
-      class="w-full p-4 md:p-0 mt-6 md:w-5/6 lg:max-w-7xl h-full flex flex-col justify-start items-center self-center"
+      class="w-full p-4 md:p-0 mt-6 md:w-5/6 lg:max-w-6xl h-full flex flex-col justify-start items-center self-center"
     >
       <img
-        class="hidden md:block w-full object-scale-down"
+        class="hidden md:block w-auto max-w-full object-scale-down"
         :src="state.post?.feature_image || ''"
         :alt="state.post?.feature_image_alt || ''"
       />
       <!-- title -->
       <div
-        class="w-full max-w-6xl mt-12 prose prose-indigo prose-xl md:prose-2xl flex flex-row justify-center items-center"
+        class="w-auto max-w-6xl mt-4 md:mt-12 prose prose-indigo prose-xl md:prose-2xl flex flex-col justify-center items-center"
       >
         <h1 class="leading-16">
           {{ state.post?.title }}
         </h1>
       </div>
-      <!-- tags -->
+      <!-- author information -->
       <div
-        class="px-4 mt-6 w-full max-w-6xl flex flex-row justify-center items-center"
-      >
-        <span
-          v-for="tag in state.post?.tags"
-          :key="tag.id"
-          class="items-center px-3 py-0.5 mr-2 rounded-full text-base font-medium"
-        >
-          # {{ tag.name }}
-        </span>
-      </div>
-      <!-- author infomartion -->
-      <div
-        class="w-full mt-2 mb-4 max-w-6xl px-2 flex flex-row items-center justify-center text-sm text-gray-900 font-semibold tracking-wide uppercase"
+        class="w-full mt-8 mb-2 max-w-6xl px-2 flex flex-row items-center justify-center text-sm text-gray-900 font-semibold tracking-wide uppercase"
       >
         <img
           class="h-8 w-auto rounded-full mr-2"
           :src="state.post?.authors![0].profile_image || ''"
         />{{ state.post?.authors![0].name }}
-        <div class="flex space-x-1 text-gray-500">
+        <div class="ml-2 flex space-x-1 text-gray-500">
           <time :datetime="state.post?.published_at || ''">
             {{
               new Date(state.post?.published_at || "").toLocaleString(
@@ -53,6 +41,18 @@
           <span aria-hidden="true"> &middot; </span>
           <span> {{ state.post?.reading_time }} min read </span>
         </div>
+      </div>
+      <!-- blog tags -->
+      <div
+        class="mb-8 w-full max-w-6xl flex flex-row justify-center items-center"
+      >
+        <span
+          v-for="tag in state.post?.tags"
+          :key="tag.id"
+          class="items-center px-3 py-0.5 mr-2 rounded-full text-base"
+        >
+          # {{ tag.name }}
+        </span>
       </div>
       <div
         class="w-full max-w-5xl prose prose-indigo prose-xl md:prose-2xl"
