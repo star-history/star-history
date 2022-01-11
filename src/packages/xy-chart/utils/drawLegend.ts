90f07a8f82cd0e41acb5a5d7128cfc330c154464
email <imrealleonardo@gmail.com>
2022-01-11 08:38:27 +0800

    get bounding by text length

--- src/packages/xy-chart/utils/drawLegend.ts
+++ src/packages/xy-chart/utils/drawLegend.ts
@@ -13,9 +13,15 @@ const drawLegend = (
   selection: D3Selection,
   { items, strokeColor, backgroundColor }: DrawLegendConfig
 ) => {
+  const legendPadding = 10;
+  const xkcdCharWidth = 7;
+  const xkcdCharHeight = 20;
+  const colorBlockWidth = 15;
+
   const legend = selection.append("svg");
   const backgroundLayer = legend.append("svg");
   const textLayer = legend.append("svg");
+  let maxTextLength = 0;
 
   items.forEach((item, i) => {
     // draw color dot
@@ -28,43 +34,37 @@ const drawLegend = (
       .attr("ry", 2)
       .attr("filter", "url(#xkcdify)")
       .attr("x", 15)
-      .attr("y", 17 + 20 * i);
+      .attr("y", 17 + xkcdCharHeight * i);
     // draw text
     textLayer
       .append("text")
       .style("font-size", "15")
       .style("fill", strokeColor)
-      .attr("x", 15 + 12)
-      .attr("y", 17 + 20 * i + 8)
+      .attr("x", colorBlockWidth + 12)
+      .attr("y", 17 + xkcdCharHeight * i + 8)
       .text(item.text);
+
+    maxTextLength = Math.max(item.text.length, maxTextLength);
   });
 
-  // NOTE: get the correct width and height after textLayer rendered
-  setTimeout(() => {
-    const bbox = textLayer.node()?.getBoundingClientRect();
-    if (!bbox) {
-      console.error("bbox get null in drawLegend");
-      return;
-    }
+  const backgroundWidth =
+    maxTextLength * xkcdCharWidth + colorBlockWidth + legendPadding;
+  const backgroundHeight = items.length * xkcdCharHeight + legendPadding;
 
-    const backgroundWidth = bbox.width + 15;
-    const backgroundHeight = bbox.height + 10;
-
-    // add background
-    backgroundLayer
-      .append("rect")
-      .style("fill", backgroundColor)
-      .attr("fill-opacity", 0.85)
-      .attr("stroke", strokeColor)
-      .attr("stroke-width", 2)
-      .attr("rx", 5)
-      .attr("ry", 5)
-      .attr("filter", "url(#xkcdify)")
-      .attr("width", backgroundWidth)
-      .attr("height", backgroundHeight)
-      .attr("x", 8)
-      .attr("y", 5);
-  }, 0);
+  // add background
+  backgroundLayer
+    .append("rect")
+    .style("fill", backgroundColor)
+    .attr("fill-opacity", 0.85)
+    .attr("stroke", strokeColor)
+    .attr("stroke-width", 2)
+    .attr("rx", 5)
+    .attr("ry", 5)
+    .attr("filter", "url(#xkcdify)")
+    .attr("width", backgroundWidth)
+    .attr("height", backgroundHeight)
+    .attr("x", 8)
+    .attr("y", 5);
 };
 
 export default drawLegend;
