ab049f6a8edd3d6d99ba6d717762131e8815f3ec
STEVEN <imrealleonardo@gmail.com>
2022-01-27 10:36:32 +0800

    feat: update build scripts (#126)
    * feat: update build scripts
    * fix: re-yarn
    * chore: add a comment
    * Update vite.config.ts
    
    Co-authored-by: tianzhou <t@bytebase.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig, UserConfigExport } from "vite";
 import vue from "@vitejs/plugin-vue";
-import generateSitemap from "./scripts/generateSitemap";
 import copyExtensionFiles from "./scripts/copyExtensionFiles";
+import emptyOutDir from "./scripts/emptyOutDir";
+import generateSitemap from "./scripts/generateSitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+  // Control the building behavior and make its first step to empty the output folder.
+  // This is to make sure the other script can work right.
+  emptyOutDir();
+
   const config: UserConfigExport = {
     plugins: [vue()],
-    build: {
-      emptyOutDir: true,
-    },
+    build: {},
   };
 
   if (mode === "extension") {
