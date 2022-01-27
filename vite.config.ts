d41cc914be6618ece5625466158e577a6b48d087
email <imrealleonardo@gmail.com>
2022-01-27 09:59:30 +0800

    chore: add a comment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import generateSitemap from "./scripts/generateSitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+  // Control the building behavior and make its first step is to empty the output folder.
+  // To make sure the other script can works right.
   emptyOutDir();
 
   const config: UserConfigExport = {
