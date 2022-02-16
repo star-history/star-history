3f2dad1267d3f3e4728107613f795218309c636e
email <imrealleonardo@gmail.com>
2022-02-16 15:35:05 +0800

    feat: logger based on winston

---
+++ server/logger.ts
@@ -0,0 +1,26 @@
+import * as fs from "fs";
+import * as path from "path";
+import winston from "winston";
+
+const createLogger = () => {
+  const logFilename = path.join(__dirname, "server.log");
+
+  // Remove the logger file, ignoring any errors.
+  try {
+    fs.unlinkSync(logFilename);
+  } catch (ex) {
+    // do nth
+  }
+
+  return winston.createLogger({
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({
+        filename: logFilename,
+      }),
+    ],
+    format: winston.format.combine(winston.format.simple()),
+  });
+};
+
+export default createLogger();
