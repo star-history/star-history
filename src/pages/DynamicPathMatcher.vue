2767e97aac213d5ce0c1810aab07e33e780ef70e
email <imrealleonardo@gmail.com>
2022-03-07 23:14:32 +0800

    feat: dynamic path matcher

---
+++ src/pages/DynamicPathMatcher.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>
+    <!-- do nth -->
+  </p>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
+
+onMounted(() => {
+  const pathList = route.params.pathList;
+  if (Array.isArray(pathList) && pathList.length >= 2) {
+    router.push({ path: `/`, hash: `#${pathList[0]}/${pathList[1]}` });
+  } else {
+    router.push({ path: "/404" });
+  }
+});
+</script>
