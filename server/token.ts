6200801dadf441f6ca38c8d5e159530cc3b3166c
email <imrealleonardo@gmail.com>
2022-03-10 20:09:54 +0800

    (test) relative file path for render

--- server/token.ts
+++ server/token.ts
@@ -3,14 +3,22 @@ import * as process from "process";
 import logger from "./logger";
 import api from "../common/api";
 
-// All secret files in render are available to load by absolute path at /etc/secrets/<filename>.
-const ENV_PATH_IN_RENDER = "/etc/secrets/token.env";
+// Token env file path in render.com: All secret files you create are available to read at the root of your repo.
+const ENV_PATH_IN_RENDER = "./token.env";
+// For local dev, you need to pass the ENVPATH value in env variables.
+// e.g. ENVPATH=PATH_TO_YOUR_FILE yarn dev
+// For production, we set a token.env file in render.com,
+// and copy of the file is store in bytebase/secret repo on GitHub.
+const envFilePath = process.env.ENVPATH || ENV_PATH_IN_RENDER;
 
 const savedTokens: string[] = [];
 let index = 0;
 
 export const initTokenFromEnv = async () => {
-  const envFilePath = process.env.ENVPATH || ENV_PATH_IN_RENDER;
+  if (!fs.existsSync(envFilePath)) {
+    logger.error("Token file not found with path ", envFilePath);
+    process.exit(-1);
+  }
   const envTokenString = fs.readFileSync(envFilePath).toString();
   if (!envTokenString) {
     logger.error("Token not found");
