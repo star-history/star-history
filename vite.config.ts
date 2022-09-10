1b54dcb5076a19ea4d79b7c8a5c436f78ed8a98a
ZhengX <56376387+Megrax@users.noreply.github.com>
2022-09-11 01:06:58 +0800

    chore: let vite try another port while :3000 is occupied (#207)

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import additionBuildPlugin from "./plugins/additionBuildPlugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), additionBuildPlugin()],
+  server: {
+    host: "0.0.0.0",
+  },
 });
