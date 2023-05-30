8c5728b11e4e5f7e1153da6e8188bc4e1a7f15a2
Mila Wu <wumila1996@gmail.com>
2023-05-30 11:21:39 +0800

    add april selections
    Signed-off-by: Mila Wu <wumila1996@gmail.com>

--- src/components/HighlightBlogSection.vue
+++ src/components/HighlightBlogSection.vue
@@ -49,6 +49,10 @@ const blogSectionList = [
     title: "Monthly Pick",
     linkList: [
       {
+        title: "2023 Apr",
+        path: "https://star-history.com/blog/star-history-monthly-pick-202304",
+      },
+      {
         title: "2023 Mar (ChatGPT Edition)",
         path: "https://star-history.com/blog/star-history-monthly-pick-202303",
       },
