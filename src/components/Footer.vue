fc9e1d463d9d5288a43b5a21d983d717f3711db1
Tianzhou Chen <t@bytebase.com>
2023-07-01 15:37:58 +0800

    chore: change image path

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -37,13 +37,13 @@
         <div
           class="flex flex-row text-sm leading-8 underline text-blue-700 hover:opacity-80"
         >
-          <img class="h-6 mt-1 mr-2" src="/public/sqlchat.webp" />
+          <img class="h-6 mt-1 mr-2" src="/sqlchat.webp" />
           <a href="https://sqlchat.ai" target="_blank"> SQL Chat </a>
         </div>
         <div
           class="flex flex-row text-sm leading-8 underline text-blue-700 hover:opacity-80"
         >
-          <img class="h-6 mt-1 mr-2" src="/public/dbcost.webp" />
+          <img class="h-6 mt-1 mr-2" src="/dbcost.webp" />
           <a href="https://dbcost.com" target="_blank">DB Cost</a>
         </div>
       </div>
