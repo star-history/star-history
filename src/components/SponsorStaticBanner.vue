c62798722055aa279923f2c0aa3aa0da7b72cf15
Tianzhou Chen <t@bytebase.com>
2023-08-05 14:46:52 +0800

    chore: remove ads boarder

--- src/components/SponsorStaticBanner.vue
+++ src/components/SponsorStaticBanner.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="fixed right-0 top-32 hidden lg:flex flex-col justify-start items-start transition-all bg-white w-48 xl:w-56 shadow p-4 rounded z-50"
+    class="fixed right-0 top-32 hidden lg:flex flex-col justify-start items-start transition-all bg-white w-48 xl:w-56 p-2 z-50"
     :class="state.hide && '!hidden'"
   >
-    <div class="w-full flex flex-row justify-between items-center mb-2 -mt-2">
+    <div class="w-full flex flex-row justify-between items-center mb-2">
       <span class="text-xs text-gray-400">SPONSORS</span>
       <i
-        class="fas fa-times text-base text-gray-400 cursor-pointer hover:text-gray-500"
+        class="fas fa-times text-xs text-gray-400 cursor-pointer hover:text-gray-500"
         @click.prevent="handleCloseButtonClick"
       ></i>
     </div>
