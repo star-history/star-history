2bb4f5acfd298306f773c1dda6187343c775f380
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2024-01-31 13:17:46 +0200

    changed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,37 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "RGB(54, 54, 54)",
+        light: "RGB(245, 245, 245)",
+      },
+      minWidth: {
+        "600px": "600px",
+      },
+      minHeight: {
+        "400px": "400px",
+      },
+      boxShadow: {
+        focus: "0 0 0 0.125em rgb(54 54 54 / 25%)",
+      },
+      zIndex: {
+        100: "100",
+      },
+      spacing: {
+        52: "13rem",
+        112: "28rem",
+        128: "32rem",
+        152: "38rem",
+        160: "40rem",
+        176: "44rem",
+        192: "48rem",
+        208: "52rem",
+      },
+    },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
