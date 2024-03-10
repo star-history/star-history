15471300ebe883d0c4dae65d61c2b4f06b20a54b
Tianzhou Chen <t@bytebase.com>
2024-03-10 17:10:44 +0800

    chore: remove blog/slug loading

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -24,12 +24,11 @@ interface Blog {
 }
 
 interface State {
-    isLoading: boolean
-    blog?: Blog
+    blog: Blog
     parsedBlogHTML?: string
 }
 
-const BlogPost: React.FC<State> = ({ isLoading, blog, parsedBlogHTML }) => {
+const BlogPost: React.FC<State> = ({ blog, parsedBlogHTML }) => {
     return (
         <AppStateProvider>
             <div className="relative w-full h-auto min-h-screen overflow-auto flex flex-col">
@@ -40,47 +39,30 @@ const BlogPost: React.FC<State> = ({ isLoading, blog, parsedBlogHTML }) => {
                         <HighlightBlogSection />
                     </div>
                     <div className="w-full flex flex-col justify-start items-center">
-                        {isLoading ? (
-                            <div className="grow w-full flex flex-col justify-center items-center">
-                                <FaSpinner className="animate-spin text-4xl z-10" />
+                        <div className="w-full p-4 md:p-0 mt-6 md:w-5/6 lg:max-w-6xl h-full flex flex-col justify-start items-center self-center">
+                            <img className="hidden md:block w-auto max-w-full object-scale-down" src={blog.featureImage || ""} alt="" />
+                            <div className="w-auto max-w-6xl mt-4 md:mt-12 prose prose-indigo prose-xl md:prose-2xl flex flex-col justify-center items-center">
+                                <h1 className="leading-16">{blog.title}</h1>
                             </div>
-                        ) : blog === undefined ? (
-                            <div className="w-full h-10 flex flex-col justify-center items-center">
-                                <p className="text-center leading-8 text-lg text-dark font-medium">Oops! No article found.</p>
-                                <p className="text-center leading-8 text-lg text-dark font-medium">
-                                    <Link href="/blog">
-                                        <button className="w-full px-4 py-2 h-full text-base rounded-md bg-gray-400 shadow-inner text-light hover:bg-gray-500">
-                                            <i className="fas fa-chevron-left mr-1"></i>
-                                            Back to blog list
-                                        </button>
-                                    </Link>
-                                </p>
-                            </div>
-                        ) : (
-                            <div className="w-full p-4 md:p-0 mt-6 md:w-5/6 lg:max-w-6xl h-full flex flex-col justify-start items-center self-center">
-                                <img className="hidden md:block w-auto max-w-full object-scale-down" src={blog.featureImage || ""} alt="" />
-                                <div className="w-auto max-w-6xl mt-4 md:mt-12 prose prose-indigo prose-xl md:prose-2xl flex flex-col justify-center items-center">
-                                    <h1 className="leading-16">{blog.title}</h1>
-                                </div>
-                                <div className="w-full mt-8 mb-2 max-w-6xl px-2 flex flex-row items-center justify-center text-sm text-gray-900 font-semibold trackingwide uppercase">
-                                    <div className="flex space-x-1 text-gray-500">
-                                        <span className="text-gray-900">{blog.author}</span>
-                                        <span aria-hidden="true"> &middot; </span>
-                                        <time dateTime={blog.publishedDate}>
-                                            {new Date(blog.publishedDate || "").toLocaleDateString("en-US", {
-                                                year: "numeric",
-                                                month: "short",
-                                                day: "numeric"
-                                            })}
-                                        </time>
+                            <div className="w-full mt-8 mb-2 max-w-6xl px-2 flex flex-row items-center justify-center text-sm text-gray-900 font-semibold trackingwide uppercase">
+                                <div className="flex space-x-1 text-gray-500">
+                                    <span className="text-gray-900">{blog.author}</span>
+                                    <span aria-hidden="true"> &middot; </span>
+                                    <time dateTime={blog.publishedDate}>
+                                        {new Date(blog.publishedDate || "").toLocaleDateString("en-US", {
+                                            year: "numeric",
+                                            month: "short",
+                                            day: "numeric"
+                                        })}
+                                    </time>
 
-                                        <span aria-hidden="true"> &middot; </span>
-                                        <span> {blog.readingTime} min read </span>
-                                    </div>
+                                    <span aria-hidden="true"> &middot; </span>
+                                    <span> {blog.readingTime} min read </span>
                                 </div>
-                                <div className="mt-8 w-full max-w-5xl prose prose-indigo prose-xl md:prose-2xl" dangerouslySetInnerHTML={{ __html: parsedBlogHTML || "" }} />
                             </div>
-                        )}
+                            <div className="mt-8 w-full max-w-5xl prose prose-indigo prose-xl md:prose-2xl" dangerouslySetInnerHTML={{ __html: parsedBlogHTML || "" }} />
+                        </div>
+                
                         <div className="mt-12">
                             <iframe
                                 src="https://embeds.beehiiv.com/2803dbaa-d8dd-4486-8880-4b843f3a7da6?slim=true"
@@ -109,7 +91,6 @@ const BlogPost: React.FC<State> = ({ isLoading, blog, parsedBlogHTML }) => {
 export async function getServerSideProps(context: GetServerSidePropsContext) {
     let returnObj = {
         props: {
-            isLoading: true,
             blog: null as Blog | null,
             parsedBlogHTML: ""
             // readingTime: 0,
@@ -132,10 +113,9 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
         const wordCount = content.split(" ").length
         const readingTime = Math.ceil(wordCount / wordsPerMinute)
 
-        // Update return object with blog data, reading time, and set isLoading: false
+        // Update return object with blog data, reading time
         returnObj = {
             props: {
-                isLoading: false,
                 blog: {
                     ...blog,
                     readingTime: readingTime // Add reading time to the blog object
