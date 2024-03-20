1bfa353d827ea20d949cba393b9a8a79ef6d1912
tianzhou <t@bytebase.com>
2024-03-20 17:16:15 +0800

    chore: use setVisible for github star instead of loading icon

--- components/GitHubStarButton.tsx
+++ components/GitHubStarButton.tsx
@@ -3,6 +3,7 @@ import axios from "axios"
 import { FaGithub } from "react-icons/fa"
 
 const GitHubStarButton = () => {
+    const [visible, setVisible] = useState(false) 
     const [starCount, setStarCount] = useState(0)
 
     useEffect(() => {
@@ -16,8 +17,9 @@ const GitHubStarButton = () => {
                     }
                 })
                 count = data.stargazers_count;
+                setVisible(true)
             } catch (error) {
-              // 
+              console.error('Failed to fetch GitHub star', error)
             }
             return count;
         }
@@ -26,18 +28,22 @@ const GitHubStarButton = () => {
     }, [])
 
     return (
-        <a
-            className="border rounded flex flex-row justify-start items-center text-black bg-white text-sm shadow-inner hover:opacity-80"
-            href="https://github.com/star-history/star-history"
-            target="_blank"
-            aria-label="Star star-history/star-history on GitHub"
-        >
-            <span className="pr-1 pl-2 h-full flex flex-row justify-center items-center bg-gray-100 border-r font-medium">
-                <FaGithub className="fab text-base mr-1 -mt-px" />
-                <span className="mt-px">Star</span>
+        <>
+        { visible && (
+            <a
+                className="border rounded flex flex-row justify-start items-center text-black bg-white text-sm shadow-inner hover:opacity-80"
+                href="https://github.com/star-history/star-history"
+                target="_blank"
+                aria-label="Star star-history/star-history on GitHub"
+            >
+                <span className="pr-1 pl-2 h-full flex flex-row justify-center items-center bg-gray-100 border-r font-medium">
+                    <FaGithub className="fab text-base mr-1 -mt-px" />
+                    <span className="mt-px">Star</span>
             </span>
-            <div className="h-full block px-2 mt-px font-medium">{starCount === 0 ? <i className="fa fa-spinner animate-spin opacity-90 px-2"></i> : <span>{starCount}</span>}</div>
-        </a>
+            <div className="h-full block px-2 mt-px font-medium">{starCount}</div>
+            </a>
+        )}
+        </>
     )
 }
 
