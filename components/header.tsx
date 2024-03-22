7bd1c4e48aa090806da9f55ae525329553045192
tianzhou <t@bytebase.com>
2024-03-22 17:16:28 +0800

    chore: open forum in a new tab

--- components/header.tsx
+++ components/header.tsx
@@ -56,9 +56,9 @@ useEffect(() => {
               <Link href="/" className="h-full flex flex-row justify-center items-center px-4 hover:bg-zinc-800">
                 <img className="w-7 h-auto" src="/assets/icon.png" alt="Logo" />
               </Link>
-              <Link href="https://forum.star-history.com" className="h-full flex flex-row justify-center items-center text-base px-5 hover:bg-zinc-800">
+              <a href="https://forum.star-history.com" target="__blank" className="h-full flex flex-row justify-center items-center text-base px-5 hover:bg-zinc-800">
                 <span className="text-white font-semibold -2">Forum</span>
-              </Link>
+              </a>
               <span
                 className="h-full flex flex-row justify-center items-center cursor-pointer text-white text-base px-3 font-semibold mr-2 hover:bg-zinc-800"
                 onClick={handleSetTokenBtnClick}
