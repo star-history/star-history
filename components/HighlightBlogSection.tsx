63426bb3bac655686d830b0eb17f462e48130ef6
OP3ratordec <1914592563@qq.com>
2024-11-20 18:37:57 +0800

    docs: add Nov. monthly

--- components/HighlightBlogSection.tsx
+++ components/HighlightBlogSection.tsx
@@ -33,6 +33,10 @@ const blogSectionList: BlogSection[] = [
         title: "Monthly Pick",
         linkList: [
             {
+                title: "2024 Nov (AI DevTools)",
+                path: "/blog/ai-devtools",
+            },
+            {
                 title: "2024 Oct (Homelab)",
                 path: "/blog/homelab",
             },
