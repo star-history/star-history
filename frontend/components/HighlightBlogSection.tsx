20bdaf1b8307df33b940d1d74a56beab520f58be
tianzhou <t@bytebase.com>
2025-03-31 15:11:33 +0800

    chore: tweak wording

--- frontend/components/HighlightBlogSection.tsx
+++ frontend/components/HighlightBlogSection.tsx
@@ -307,7 +307,7 @@ const HighlightBlogSection: React.FC<Omit<HighlightBlogSectionProps, "blogSectio
         <div className="flex flex-col justify-start items-start w-full mt-2 p-4 pl-8">
             <Link href="/blog/list-your-open-source-project" className="flex flex-col items-center group transition-all duration-200">
                 <img className="w-auto max-w-full" src="/assets/starlet-icon.webp" alt="Starlet Icon" />
-                <span className="text-sm text-blue-700 mt-1 group-hover:underline">👆 List your project for FREE</span>
+                <span className="text-sm text-blue-700 mt-1 group-hover:underline">👆 Show your project for FREE</span>
             </Link>
             {blogSectionList.map((section) => (
                 <div key={section.title}>
